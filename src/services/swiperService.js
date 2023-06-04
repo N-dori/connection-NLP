90974c8a155bae4da542699bd91f1a722452e310
Nadav Dori <dori.nadav@gmail.com>
2023-06-04 14:17:56 +0300

    add carousel for home cmp

---
+++ src/services/swiperService.js
@@ -0,0 +1,23 @@
+export const  slides = [
+    {
+        id:'i100',
+        image:'https://cdn.educba.com/academy/wp-content/uploads/2019/04/What-is-NLP-1.jpg',
+        title:'NLP-practice',
+        subTitle:'npl for biggeners',
+        interval:1500
+    },
+    {
+        id:'i101',
+        image:'https://www.xoriant.com/cdn/ff/yH8KFP9UpFRhaXz2jeKsn19NXIVb2Gmui0VBSTlPRHA/1622011601/public/2021-05/Xoriant-Natural-Language-Processing.png',
+        title:'Nlp-master',
+        subTitle:'nlp nlp nlp course',
+        interval:1500
+    },
+    {
+        id:'i102',
+        image:'https://uploads-ssl.webflow.com/5f8adf96ff635f2728f2e3e4/622f5ab1c267053a030b6e67_NLP%20Tools_Main.png',
+        title:'NLP- intermideat',
+        subTitle:'loram ipsom ipgys',
+        interval:1500
+    },
+]
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { imgService } from '../services/imgService'
+
 import { CoursesIndex } from '../cmps/CoursesIndex'
+import { SwiperCarousel } from '../cmps/SwiperCarousel'
+import { slides } from '../services/swiperService';
+
 export  function Home() {
 
   
 const loggdingUser = useSelector((storeState) => storeState.userModule.loggdingUser)
-console.log("loggdingUser",loggdingUser);
+// console.log("loggdingUser",loggdingUser);
+
   return (
-    <section className='home-page-container'>
-        <section className='hero-img-container'>
-    {loggdingUser?
+    <section className='home-page-container grid'>
+      <section className='carousel-container'>
+              {loggdingUser?
      <p className='home-greeting'>hello {loggdingUser.fullname?loggdingUser.fullname:loggdingUser.fname}</p>
      :<div>Hello Guest</div>}
-
-       </section>
+     <SwiperCarousel slides={slides}/>
+      </section>
        <section className='title-container'>
         <h1>A broad selection of courses</h1>
         <h3>Choose from over 210,000 online video courses with new additions published every month</h3>
-       <CoursesIndex/>
+            <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, tempora quidem! Dignissimos, unde adipisci. Obcaecati temporibus id totam, ut dignissimos eligendi a. Distinctio adipisci, eum saepe voluptatem laboriosam nulla quis.</h4>
         </section> 
+       <CoursesIndex/>
 
     </section>
   
