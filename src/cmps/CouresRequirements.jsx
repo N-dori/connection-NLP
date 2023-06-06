717a362c390d98d79c1f9148845ca083d8555816
Nadav Dori <dori.nadav@gmail.com>
2023-06-06 10:53:15 +0300

    add to course details requirments and description

---
+++ src/cmps/CouresRequirements.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export  function CouresRequirements() {
+  return (
+   
+   
+        <div className='course-requierments-warpper flex-ac'>
+
+        <h1 className='headline'>Requirements</h1>
+
+        <ul className="requirements-list flex-col">
+          <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda unde reprehenderit eligendi  
+          </li>
+          <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda unde reprehenderit eligendi  
+          </li>
+          <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda unde reprehenderit eligendi  
+          </li>
+        </ul>
+        </div>
+        
+
+  
+  )
+}
