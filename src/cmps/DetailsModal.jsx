503494bfaf89ae9cfe75ba4df1187aa8ec2575a3
Nadav Dori <dori.nadav@gmail.com>
2023-06-06 22:36:44 +0300

    add to detials cmp call to action  modal and sticky header

--- src/cmps/DetailsModal.jsx
+++ src/cmps/DetailsModal.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { imgService } from '../services/imgService'
+import { PlaySvg } from '../svgs/PlaySvg'
 
-export  function DetailsModal() {
+export function DetailsModal() {
+
+
+
   return (
-    <>
-    {/* <YouTube videoUrl={course.TrailerVideoUrl}/> */}
-    <div>courseDetailsModal</div>
-    
-    </>
+
+
+    <section className='details-modal-container'>
+
+      <section className="modal-warpper">
+        <div className='preview-course-img-wrapper' 
+      >
+          <div className='green'></div>
+          <div className='sand'>
+
+          </div>
+          <div className='black flex-jc'>
+            <p>preview this course</p>
+          </div>
+          <div className='laptop-img-container'>
+            <img className='laptop-img' src={imgService.getImg('laptop')} />
+            <PlaySvg />
+          </div>
+        </div>
+        <section className='actions-section'>
+        <p className='headline'> Make sure to sign up first</p>
+        <p>Get this life changing course, and get up to 10% discount </p>
+        <div className='price'>$444.90</div>
+          <div className='action-btn-container flex-jc'>
+        <button className='action-btn flex-jc-ac'><span>Buy this course</span></button>
+
+          </div>
+        <p className='full-life-time'>Full Lifetime Access</p>
+
+        </section>
+
+      </section>
+    </section>
+
+
   )
 }
