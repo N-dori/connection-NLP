abb794d63b1b7f437bee0e2ed9dd01a8a40f113b
Nadav Dori <dori.nadav@gmail.com>
2023-06-06 09:42:58 +0300

    add first panel in course content stay open all time

--- src/cmps/CourseContent.jsx
+++ src/cmps/CourseContent.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { ExpandSvg } from '../svgs/ExpandSvg'
 import { Panel } from './Panel'
+import { FirstPanel } from './FirstPanel'
 
 export  function CourseContent() {
     const [isAllExpaned,setIsAllExpand]= useState(false)
@@ -13,9 +14,9 @@ export  function CourseContent() {
         <h1 className='headline'>Course content</h1>
         <section className='course-curriculum flex-sb'>
             <div className='info flex-ac'>16 sections • 51 lectures • 9h 30m total length</div>
-            <button onClick={expandAll} className='expend-all-btn flex-ac'>{isAllExpaned?'Expand All Sections':'Collapse All Sections'}</button>
+            <button onClick={expandAll} className='expend-all-btn flex-ac'>{isAllExpaned?'Collapse All Sections':'Expand All Sections'}</button>
         </section>
-       <Panel title={'intro'} isAllExpaned={isAllExpaned} lectures={3}min={27}/>
+        <FirstPanel title={'intro'} isAllExpaned={isAllExpaned} lectures={3}min={27}/>
        <Panel title={'basics'} isAllExpaned={isAllExpaned} lectures={4}min={38}/>
        <Panel title={'flow'} isAllExpaned={isAllExpaned} lectures={3}min={38}/>
        <Panel title={'life not easy'} isAllExpaned={isAllExpaned} lectures={5}min={55}/>
