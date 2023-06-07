8b239137a18aed611bf421e78bd3f526e464ec6d
Nadav Dori <dori.nadav@gmail.com>
2023-06-07 20:58:50 +0300

    add course player cmp

---
+++ src/svgs/XSvg.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export  function XSvg() {
+  return (
+<svg className='x-svg' xmlns="http://www.w3.org/2000/svg" height="25" viewBox="0 -960 960 960" width="25"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>
+  )
+}
